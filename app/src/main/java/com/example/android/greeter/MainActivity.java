e60a491b04ddcd4a6e8586a6c02686df28902ae6
BermetBatieva <bermetbatieva@gmail.com>
2020-10-04 01:08:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android/greeter/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.greeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void PressGreetButton(View view) {
+        EditText greetEditText =
+                (EditText) findViewById(R.id.greetEditText);
+
+        String name = greetEditText.getText().toString();
+        String greeting = String.format("Hello, %s", name);
+
+        TextView messageTextView =
+                (TextView) findViewById(R.id.messageTextView);
+
+        messageTextView.setText(greeting);
+    }
+}
